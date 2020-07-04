0419bb8a1eb820ac199042e5c09d073a74a8702a
JunIshino <jun.ishino@kepple.jp>
2020-07-05 00:56:16 +0900

    Home画面実装、共通化とトップ画像は適当

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,16 +2,86 @@ import React from "react";
 import index from "./index.module.scss";
 
 import { NextPage } from "next";
+import { Avatar, Button } from "@material-ui/core";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import GitHubIcon from "@material-ui/icons/GitHub";
 
 const Home: NextPage = () => {
   return (
-    <div className={index.wrapper}>
-      <p>
-        SSRだよー！!!毎回スターの数（更新されれば）とビルド時間はビルド後から変わらないよー！
-      </p>
-      <p>Next.JSのスター数：</p>
-      <p>ビルド時間：</p>
-    </div>
+    <main className={index.wrapper}>
+      <nav className={index.nav}>
+        <div className={index.container}>
+          <div className={index.contents}>
+            <a className={index.content}>
+              <div className={index.before}>
+                <p>
+                  <img src="/profile.png" alt="Profile" />
+                </p>
+              </div>
+              <span className={index.after}>Profile</span>
+            </a>
+            <a className={index.content}>
+              <span className={index.before}>
+                <img src="/portfolio.png" alt="Portfolio" />
+              </span>
+              <span className={index.after}>Portfolio</span>
+            </a>
+            <a className={index.content}>
+              <span className={index.before}>
+                <img src="/blog.png" alt="Blog" />
+              </span>
+              <span className={index.after}>Blog</span>
+            </a>
+            <a className={index.content}>
+              <span className={index.before}>
+                <img src="/contact.png" alt="Contact" />
+              </span>
+              <span className={index.after}>Contact</span>
+            </a>
+          </div>
+        </div>
+      </nav>
+      <section className={index.section}>
+        <div className={index.container}>
+          <div className={index.profile}>
+            <div className={index.titleArea}>
+              <h1 className={index.title}>JUN ISHINO</h1>
+              <h3>Web Developer</h3>
+              <div className={index.linkArea}>
+                <Button
+                  variant="contained"
+                  className={index.twitter}
+                  href="https://twitter.com/JJ_1123_I"
+                  target="_blank"
+                >
+                  <TwitterIcon />
+                </Button>
+                <Button
+                  variant="contained"
+                  className={index.github}
+                  href="https://github.com/IshinoJun"
+                  target="_blank"
+                >
+                  <GitHubIcon />
+                </Button>
+              </div>
+            </div>
+            <div className={index.iconArea}>
+              <Avatar className={index.icon} src="/test.jpeg" />
+            </div>
+          </div>
+        </div>
+      </section>
+      <footer>
+        <div className={index.container}>
+          <div className={index.footerContent}>
+            <span>
+              Jun Ishino {new Date().getFullYear()} All right reserved
+            </span>
+          </div>
+        </div>
+      </footer>
+    </main>
   );
 };
 export default Home;
