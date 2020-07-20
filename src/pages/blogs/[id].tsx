55976b1db6ebe7d695fc2758bd3a968f00c329f8
JunIshino <jun.ishino@kepple.jp>
2020-07-20 23:10:34 +0900

    markdown以外を実装

--- src/pages/blogs/[id].tsx
+++ src/pages/blogs/[id].tsx
@@ -1,12 +1,15 @@
 import { NextPage, GetStaticPaths, GetStaticProps } from "next";
 import ErrorPage from "next/error";
-import Link from "next/link";
 import * as React from "react";
 import Blog from "../../models/Blog";
 import DevClient from "../../pages/api/DevClient";
 import { isPreviewData } from "../../utils/TypeGuardUtils";
 import Layout from "../../components/shared/Layout";
 import HeaderProps from "../../models/HeaderProps";
+import style from "./id.module.scss";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import { formatDate } from "../../utils/FormatUtils";
+import Tags from "../../components/shared/Tags";
 
 interface Props {
   blog: Blog | null;
@@ -26,13 +29,26 @@ const BlogDetail: NextPage<Props> = (props: Props) => {
     <>
       {blog ? (
         <Layout title={blog.title} headerProps={headerProps}>
-          <h1 className="title">ブログ詳細</h1>
-          <Link href="/blogs/">
-            <a className="link">ブログトップへ</a>
-          </Link>
-          <div className="item">
-            <h2 className="item__title">{blog.title}</h2>
-          </div>
+          <section className="padding-block border-bottom">
+            <div className="container">
+              <div className={style.contact}>
+                <div className={style.blog}>
+                  <div className={style.photo}>
+                    <img src={blog.ogp.url} />
+                  </div>
+                  <h3>{blog.title}</h3>
+                  <div className={style.date}>
+                    <AccessTimeIcon />
+                    <span>{formatDate(new Date(blog.date))}</span>
+                  </div>
+                  <Tags tags={blog.tags} tagsPosition="left" />
+                  <div
+                    dangerouslySetInnerHTML={{ __html: `${blog.content}` }}
+                  />
+                </div>
+              </div>
+            </div>
+          </section>
         </Layout>
       ) : (
         <ErrorPage statusCode={404} />
