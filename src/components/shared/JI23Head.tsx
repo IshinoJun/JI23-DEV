53c9e95311de7914441f336e82944aaffe5c148a
JunIshino <jun.ishino@kepple.jp>
2020-07-24 01:09:56 +0900

    OGPの設定を修正

--- src/components/shared/JI23Head.tsx
+++ src/components/shared/JI23Head.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import Head from "next/head";
 import HeadProps from "../../models/HeadProps";
+import { formatOgpSetting } from "../../utils/FormatUtils";
 
 const JI23Head: React.FC<HeadProps> = (props: HeadProps) => {
   const { title, description, image, url, type } = props;
@@ -13,13 +14,13 @@ const JI23Head: React.FC<HeadProps> = (props: HeadProps) => {
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:site_name" content={title} />
-      <meta property="og:image" content={image} />
-      <meta name="twitter:card" content="summary" />
+      <meta property="og:image" content={formatOgpSetting(image, title)} />
+      <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:site" content="@JJ_1123_I" />
-      <meta name="twitter:url" content={image} />
+      <meta name="twitter:url" content={url} />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
-      <meta name="twitter:image" content={image} />
+      <meta name="twitter:image" content={formatOgpSetting(image, title)} />
       <link rel="canonical" href={url} />
     </Head>
   );
