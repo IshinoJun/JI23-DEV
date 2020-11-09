b16f2a18746c08e2ece5c96383c74db6346d6c6b
JunIshino <jun.ishino@kepple.jp>
2020-11-10 02:53:00 +0900

    404にnoindex追加

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 import { NextPage } from 'next';
+import Head from 'next/head';
 import Error from './_error';
 
 const Custom404: NextPage = () => {
-  return <Error statusCode={404} />;
+  return (
+    <>
+      <Head>
+        <meta name="robots" content="noindex" />
+      </Head>
+      <Error statusCode={404} />
+    </>
+  );
 };
 
 export default Custom404;
