66e44f6248aad6f8aa5e442224aa131df3014c84
JunIshino <jun.ishino@kepple.jp>
2020-11-11 00:22:58 +0900

    descriptionを修正

--- src/components/shared/BlogHead.tsx
+++ src/components/shared/BlogHead.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import Head from 'next/head';
+import { fromString } from 'html-to-text';
 import Blog from '../../models/Blog';
 
 interface Props {
@@ -11,18 +12,29 @@ const BlogHead: React.FC<Props> = (props: Props) => {
   const defaultTitle = 'JI23-DEV';
   const baseUrl = process.env.NEXT_PUBLIC_BASE_URL ?? '';
 
+  const getDescription = (description: string) => {
+    const body = description.trim().replace(/[ \r\n]/g, '');
+    if (body.length < 140) {
+      return body;
+    }
+
+    return `${body.substring(0, 140)}...`;
+  };
+
+  const description = getDescription(fromString(blog.content));
+
   return blog.id ? (
     <Head>
       <title>{`${blog.title} | ${defaultTitle}`}</title>
-      <meta name="Description" content={blog.introduction} />
+      <meta name="Description" content={description} />
       <meta property="og:title" content={blog.title} />
-      <meta property="og:description" content={blog.introduction} />
+      <meta property="og:description" content={description} />
       <meta
         property="og:image"
         content={`${baseUrl}/api/blogs/${blog.id}/ogp`}
       />
       <meta name="twitter:title" content={`${blog.title} | ${defaultTitle}`} />
-      <meta name="twitter:description" content={blog.introduction} />
+      <meta name="twitter:description" content={description} />
       <meta
         name="twitter:image"
         content={`${baseUrl}/api/blogs/${blog.id}/ogp`}
