8b914572127ce62ad1f5304500b43c141211d650
JunIshino <jun.ishino@kepple.jp>
2020-11-29 20:25:51 +0900

    消しちゃいけないまで消してた

---
+++ src/pages/api/blogs/index.ts
@@ -0,0 +1,23 @@
+import { NextApiResponse, NextApiRequest } from 'next';
+import { isBlogsQuery } from '../../../utils/TypeGuardUtils';
+import DevCMS from '../DevCMS';
+
+const contact = async (
+  req: NextApiRequest,
+  res: NextApiResponse,
+): Promise<void> => {
+  const devCMS = new DevCMS();
+
+  // クエリのチェック
+  if (!isBlogsQuery(req.body)) {
+    return res.status(404).end();
+  }
+
+  const blogs = await devCMS.getBlogs(req.body);
+
+  res.status(200).json(blogs);
+
+  return res.end('Contact enabled');
+};
+
+export default contact;
