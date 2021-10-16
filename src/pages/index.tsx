dc89d83c4fcd5865c86aa7ebe489d295d020d842
JunIshino <jun.ishino@kepple.jp>
2021-10-17 01:04:56 +0900

    alt変更

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import { NextPage, GetStaticProps } from 'next';
 import { Avatar } from '@material-ui/core';
-import style from './index.module.scss';
-
+import { GetStaticProps, NextPage } from 'next';
+import React from 'react';
 import HomeContent from '../components/shared/HomeContent';
 import IconButton from '../components/shared/IconButton';
 import IconButtonType from '../enums/IconButtonType';
 import SNS from '../models/SNS';
 import DevCMS from './api/DevCMS';
+import style from './index.module.scss';
 
 interface Props {
   sns: SNS;
@@ -19,7 +18,7 @@ const Home: NextPage<Props> = (props: Props) => {
   return (
     <>
       <nav className={style.nav}>
-        <div className="container">
+        <div className='container'>
           <div className={style.contents}>
             <HomeContent
               linkProps={{ href: '/profile' }}
@@ -29,7 +28,7 @@ const Home: NextPage<Props> = (props: Props) => {
                 width: 109,
                 height: 128,
               }}
-              name="Profile"
+              name='Profile'
             />
             <HomeContent
               linkProps={{ href: '/portfolio' }}
@@ -39,7 +38,7 @@ const Home: NextPage<Props> = (props: Props) => {
                 width: 128,
                 height: 107,
               }}
-              name="Portfolio"
+              name='Portfolio'
             />
             <HomeContent
               linkProps={{ href: '/blogs' }}
@@ -49,7 +48,7 @@ const Home: NextPage<Props> = (props: Props) => {
                 width: 129,
                 height: 124,
               }}
-              name="Blogs"
+              name='Blogs'
             />
             <HomeContent
               linkProps={{ href: '/contact' }}
@@ -59,13 +58,13 @@ const Home: NextPage<Props> = (props: Props) => {
                 width: 93,
                 height: 128,
               }}
-              name="Contact"
+              name='Contact'
             />
           </div>
         </div>
       </nav>
-      <section className="section">
-        <div className="container">
+      <section className='section'>
+        <div className='container'>
           <div className={style.profile}>
             <div className={style.titleArea}>
               <h1 className={style.title}>Jun Ishino</h1>
@@ -74,26 +73,22 @@ const Home: NextPage<Props> = (props: Props) => {
                 <IconButton
                   iconButtonType={IconButtonType.twitter}
                   href={sns.twitterUrl}
-                  ariaLabel="twitterのリンク"
+                  ariaLabel='twitterのリンク'
                 />
                 <IconButton
                   iconButtonType={IconButtonType.gitHub}
                   href={sns.gitHubUrl}
-                  ariaLabel="gitHubのリンク"
+                  ariaLabel='gitHubのリンク'
                 />
                 <IconButton
                   iconButtonType={IconButtonType.zenn}
                   href={sns.zennUrl}
-                  ariaLabel="zennのリンク"
+                  ariaLabel='zennのリンク'
                 />
               </div>
             </div>
             <div className={style.iconArea}>
-              <Avatar
-                className={style.icon}
-                src="/myIcon.png"
-                alt="ロボットのロゴ画像"
-              />
+              <Avatar className={style.icon} src='/myIcon.png' alt='ロゴ画像' />
             </div>
           </div>
         </div>
