14efdd42581fc7c32605be4f377e80914dac2c91
JunIshino <jun.ishino@kepple.jp>
2021-10-16 20:32:07 +0900

    next-env変わってた

--- next-env.d.ts
+++ next-env.d.ts
@@ -2,5 +2,17 @@
 /// <reference types="next/types/global" />
 /// <reference types="next/image-types/global" />
 
-// NOTE: This file should not be edited
-// see https://nextjs.org/docs/basic-features/typescript for more information.
+interface Window {
+  // pageviewのため
+  gtag(type: 'config', googleAnalyticsId: string, { page_path: string });
+  // eventのため
+  gtag(
+    type: 'event',
+    eventAction: string,
+    fieldObject: {
+      event_label: string;
+      event_category: string;
+      value?: string;
+    },
+  );
+}
