e88654fa0ed52b8c2e39b822afaca2de486a2178
JunIshino <jun.ishino@kepple.jp>
2022-09-26 01:24:38 +0900

    SEO周り封印

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,40 +2,15 @@
 import { ServerStyleSheets } from '@material-ui/core';
 import Document, { Head, Html, Main, NextScript } from 'next/document';
 import * as React from 'react';
-import { GA_ID } from '../lib/gtag';
 
 class CustomDocument extends Document {
   render(): JSX.Element {
     return (
       <Html lang='ja-JP'>
         <Head prefix='og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#'>
-          {/* Google Analytics */}
-          {GA_ID && (
-            <>
-              <script
-                async
-                src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
-              />
-              <script
-                // eslint-disable-next-line react/no-danger
-                dangerouslySetInnerHTML={{
-                  __html: `
-                  window.dataLayer = window.dataLayer || [];
-                  function gtag(){dataLayer.push(arguments);}
-                  gtag('js', new Date());
-                  gtag('config', '${GA_ID}', {
-                    page_path: window.location.pathname,
-                  });`,
-                }}
-              />
-            </>
-          )}
-          <meta
-            name='google-site-verification'
-            content={process.env.NEXT_PUBLIC_GOOGLE_SEARCH_ID}
-          />
           <link rel='apple-touch-icon' sizes='180x180' href='/icon.png' />
           <link rel='icon' type='image/png' href='/icon.png' sizes='192x192' />
+          <meta name='robots' content='noindex' />
         </Head>
         <body>
           <Main />
